760d624a9a9836bb8988a1938ac8b58b8dcfd36b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-09-12 13:40:09 +0800

    added return type of ok and not_impl

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,13 @@
 import { HttpStatus } from '@nestjs/common';
+import { Response } from 'express-serve-static-core';
 
-export function not_impl (res?): any {
+export function not_impl (res?: Response): Response | any {
   if (res) {
     return res.status(HttpStatus.NOT_IMPLEMENTED).json('not_impl');
   }
   throw new Error('not_impl');
 }
 
-export function ok (res, data) {
+export function ok (res: Response, data: object): Response {
   return res.status(HttpStatus.OK).json(data);
 }
